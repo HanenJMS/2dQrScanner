3b7bb793f061840872f32374faaafba076a4d2e5
HanenJMS <Hanensgamelab@gmail.com>
2023-12-15 01:31:00 -0500

    Adding permissions for camera usage for debugging on windows and android. Adding a Barcode scanning logic and camera view page.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -5,6 +5,7 @@
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(View.CameraViewPage), typeof(View.CameraViewPage));
         }
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Camera.MAUI;
+using Microsoft.Extensions.Logging;
 
 namespace _2dQrScanner
 {
@@ -9,6 +10,7 @@ namespace _2dQrScanner
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCameraView()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
